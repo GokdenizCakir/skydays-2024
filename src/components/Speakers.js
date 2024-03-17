8f535034ecb5c6ccf94ebe52cd843f01952a011b
GokdenizCakir <gokdenizckr@gmail.com>
2024-03-17 13:27:13 +0300

    made responsive

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -11,15 +11,21 @@ const Speakers = () => {
       <h1 className='text-center text-3xl sm:text-6xl lg:text-8xl font-semibold'>
         Konuşmacılar
       </h1>
-      <div className='grid grid-cols-2 sm:grid-cols-3 gap-4 lg:gap-16 mt-6 sm:mt-10 lg:mt-16 px-4 sm:px-24 lg:px-40'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 gap-2 sm:gap-4 lg:gap-16 mt-6 sm:mt-10 lg:mt-16 px-2 sm:px-12 lg:px-40'>
         <div className='flex flex-col justify-center items-center'>
           <div className='overflow-hidden shadow-2xl shadow-[#2f2342] rounded-[50%] flex justify-center items-center h-32 md:h-44 aspect-square'>
             <img src={eren} alt='eren törümküney' />
           </div>
-          <h2 className='text-2xl'>Eren Törümküney</h2>
-          <h3>Yapı Kredi Teknolojileri</h3>
-          <h3>Platform Güvenliği Müdürü</h3>
-          <h4>“Finans Sektöründe Siber Güvenlik”</h4>
+          <h2 className='sm:text-2xl text-nowrap'>Eren Törümküney</h2>
+          <h3 className='text-xs sm:text-base text-nowrap'>
+            Yapı Kredi Teknolojileri
+          </h3>
+          <h3 className='text-xs sm:text-base text-nowrap'>
+            Platform Güvenliği Müdürü
+          </h3>
+          <h4 className='text-xs sm:text-base text-nowrap'>
+            “Finans Sektöründe Siber Güvenlik”
+          </h4>
         </div>
         <div className='flex justify-center items-center'>
           <div className='overflow-hidden shadow-2xl shadow-[#2f2342] rounded-[50%] flex justify-center items-center h-32 md:h-44 aspect-square'>
